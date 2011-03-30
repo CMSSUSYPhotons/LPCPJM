6eef3b269d83aefb232e33b6e595f52998d863f5
dmason <>
2011-03-30 22:07:04 +0000

    first pass at full* 5e32 menu gen configs with slha input tested with 420pre8
    * albeit with a few paths removed -- DoubleMu05 a couple BTag guys, a few other muon related guys and the Alca + commissioning paths.  HLT5e32menu4.2 is based on /online/collisions/2011/5e32/v4.2/HLT/V1

---
+++ MCProduction/python/slhafragment_cff.py
@@ -0,0 +1,46 @@
+import FWCore.ParameterSet.Config as cms
+
+source = cms.Source("EmptySource")
+
+from Configuration.Generator.PythiaUEZ2Settings_cfi import *
+generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    maxEventsToPrint = cms.untracked.int32(0),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    crossSection = cms.untracked.double(3.24),
+    comEnergy = cms.double(7000.0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettingsBlock,
+        processParameters = cms.vstring('MSEL=0         ! User defined processes', 
+            'MSEL=39                  ! All SUSY processes',
+            'IMSS(1) = 11             ! Spectrum from external SLHA file',
+            'IMSS(11) = 1             ! gravitino as LSP ', 
+            'IMSS(21) = 33            ! LUN number for SLHA File (must be 33)',
+            'IMSS(22) = 33            ! Read-in SLHA decay table'),
+	SLHAParameters = cms.vstring(
+	    'SLHAFILE = winoNLSPout_800_900_800.slha'),
+        # This is a vector of ParameterSet names to be read, in this order
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters', 'SLHAParameters')
+    )
+)
+
+configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 0.1 $'),
+    name = cms.untracked.string('$Source: thisthing,v $'),
+    annotation = cms.untracked.string('SUSY GMSB something')
+)
+
+genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator"),
+    abortOnUnknownPDGCode = cms.untracked.bool(True)
+)
+
+phoFilter=cms.EDFilter("SUSYphoFilter")
+
+ProductionFilterSequence = cms.Sequence(generator*(genParticlesForFilter+phoFilter))
+
+
+
---
+++ MCProduction/python/slhafragment_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.py
@@ -0,0 +1,143 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.303 
+# Source: /cvs_server/repositories/CMSSW/CMSSW/Configuration/PyReleaseValidation/python/ConfigBuilder.py,v 
+# with command line options: LPCPJM/MCProduction/python/slhafragment_cff.py -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT:GRun --mc --conditions L1HLTST311_V0::All --beamspot Realistic7TeVCollision --datatier GEN-SIM-RAW --eventcontent RAWSIM --no_exec
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('HLT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Generator_cff')
+process.load('IOMC.EventVertexGenerators.VtxSmearedRealistic7TeVCollision_cfi')
+process.load('GeneratorInterface.Core.genFilterSummary_cff')
+process.load('Configuration.StandardSequences.SimIdeal_cff')
+process.load('Configuration.StandardSequences.Digi_cff')
+process.load('Configuration.StandardSequences.SimL1Emulator_cff')
+process.load('Configuration.StandardSequences.DigiToRaw_cff')
+#process.load('HLTrigger.Configuration.HLT_GRun_cff')
+#process.load('HLTrigger.HLTanalyzers.HLT5e32menuv12_cff')
+#process.load('HLTrigger.HLTanalyzers.HLT5e32menuv183_cff')
+process.load('LPCPJM.MCProduction.HLT5e32menuv4dot2_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("EmptySource")
+
+process.options = cms.untracked.PSet(
+
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    version = cms.untracked.string('$Revision: 0.1 $'),
+    annotation = cms.untracked.string('SUSY GMSB something'),
+    name = cms.untracked.string('$Source: thisthing,v $')
+)
+
+# Output definition
+
+process.RAWSIMoutput = cms.OutputModule("PoolOutputModule",
+    splitLevel = cms.untracked.int32(0),
+    eventAutoFlushCompressedSize = cms.untracked.int32(5242880),
+    outputCommands = process.RAWSIMEventContent.outputCommands,
+    fileName = cms.untracked.string('slhafragment_cff_py_GEN_SIM_DIGI_L1_DIGI2RAW_HLT.root'),
+    dataset = cms.untracked.PSet(
+        filterName = cms.untracked.string(''),
+        dataTier = cms.untracked.string('GEN-SIM-RAW')
+    ),
+    SelectEvents = cms.untracked.PSet(
+        SelectEvents = cms.vstring('generation_step')
+    )
+)
+
+# Additional output definition
+
+# Other statements
+#process.GlobalTag.globaltag = 'L1HLTST311_V0::All'
+process.GlobalTag.globaltag = 'START42_V8::All'
+
+process.phoFilter = cms.EDFilter("SUSYphoFilter")
+
+
+process.generator = cms.EDFilter("Pythia6GeneratorFilter",
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(7000.0),
+    crossSection = cms.untracked.double(3.24),
+    maxEventsToPrint = cms.untracked.int32(0),
+    PythiaParameters = cms.PSet(
+        pythiaUESettings = cms.vstring('MSTU(21)=1     ! Check on possible errors during program execution', 
+            'MSTJ(22)=2     ! Decay those unstable particles', 
+            'PARJ(71)=10 .  ! for which ctau  10 mm', 
+            'MSTP(33)=0     ! no K factors in hard cross sections', 
+            'MSTP(2)=1      ! which order running alphaS', 
+            'MSTP(51)=10042 ! structure function chosen (external PDF CTEQ6L1)', 
+            'MSTP(52)=2     ! work with LHAPDF', 
+            'PARP(82)=1.832 ! pt cutoff for multiparton interactions', 
+            'PARP(89)=1800. ! sqrts for which PARP82 is set', 
+            'PARP(90)=0.275 ! Multiple interactions: rescaling power', 
+            'MSTP(95)=6     ! CR (color reconnection parameters)', 
+            'PARP(77)=1.016 ! CR', 
+            'PARP(78)=0.538 ! CR', 
+            'PARP(80)=0.1   ! Prob. colored parton from BBR', 
+            'PARP(83)=0.356 ! Multiple interactions: matter distribution parameter', 
+            'PARP(84)=0.651 ! Multiple interactions: matter distribution parameter', 
+            'PARP(62)=1.025 ! ISR cutoff', 
+            'MSTP(91)=1     ! Gaussian primordial kT', 
+            'PARP(93)=10.0  ! primordial kT-max', 
+            'MSTP(81)=21    ! multiple parton interactions 1 is Pythia default', 
+            'MSTP(82)=4     ! Defines the multi-parton model'),
+        processParameters = cms.vstring('MSEL=0         ! User defined processes', 
+            'MSEL=39                  ! All SUSY processes', 
+            'IMSS(1) = 11             ! Spectrum from external SLHA file', 
+            'IMSS(11) = 1             ! gravitino as LSP ', 
+            'IMSS(21) = 33            ! LUN number for SLHA File (must be 33)', 
+            'IMSS(22) = 33            ! Read-in SLHA decay table'),
+        parameterSets = cms.vstring('pythiaUESettings', 
+            'processParameters', 
+            'SLHAParameters'),
+        SLHAParameters = cms.vstring('SLHAFILE = winoNLSPout_600_900_300.slha')
+    )
+)
+
+
+process.genParticlesForFilter = cms.EDProducer("GenParticleProducer",
+    saveBarCodes = cms.untracked.bool(True),
+    src = cms.InputTag("generator"),
+    abortOnUnknownPDGCode = cms.untracked.bool(True)
+)
+
+
+process.ProductionFilterSequence = cms.Sequence(process.generator+process.genParticlesForFilter+process.phoFilter)
+
+# Path and EndPath definitions
+process.generation_step = cms.Path(process.pgen)
+process.simulation_step = cms.Path(process.psim)
+process.digitisation_step = cms.Path(process.pdigi)
+process.L1simulation_step = cms.Path(process.SimL1Emulator)
+process.digi2raw_step = cms.Path(process.DigiToRaw)
+process.genfiltersummary_step = cms.EndPath(process.genFilterSummary)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RAWSIMoutput_step = cms.EndPath(process.RAWSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.generation_step,process.genfiltersummary_step,process.simulation_step,process.digitisation_step,process.L1simulation_step,process.digi2raw_step)
+process.schedule.extend(process.HLTSchedule)
+process.schedule.extend([process.endjob_step,process.RAWSIMoutput_step])
+# filter all path with the production filter sequence
+for path in process.paths:
+	getattr(process,path)._seq = process.ProductionFilterSequence * getattr(process,path)._seq 
